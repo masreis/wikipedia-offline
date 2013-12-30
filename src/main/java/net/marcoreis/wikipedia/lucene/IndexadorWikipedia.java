9f94396e77cd7efcf6b52742d43e1e0688168f73
Marco Reis <marco.reis@tjdft.jus.br>
2013-12-30 19:09:20 -0200

    update

---
+++ src/main/java/net/marcoreis/wikipedia/lucene/IndexadorWikipedia.java
@@ -0,0 +1,25 @@
+package net.marcoreis.wikipedia.lucene;
+
+import net.marcoreis.wikipedia.vo.PaginaWikipedia;
+
+/**
+ * Utilitário para indexação dos artigos da wikipedia.
+ * 
+ * @author marco
+ * 
+ */
+public class IndexadorWikipedia {
+
+    public IndexadorWikipedia() {
+
+    }
+
+    public void fechar() {
+
+    }
+
+    public void indexar(PaginaWikipedia pagina) {
+
+    }
+
+}
